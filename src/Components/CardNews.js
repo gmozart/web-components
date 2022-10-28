26786bf37e05f9bac1fc45b68434853caeeca75c
Gleison Mozart <mozart.gleison@gmail.com>
2022-10-28 10:30:07 -0300

    Setando as propriedades das classes

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -4,11 +4,26 @@ class CardNews extends HTMLHtmlElement{
     super();
 
     const shadow = this.attachShadow({mode:'open'});
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.style());
    
-
     }
 
-    build(){}
+    build(){
+      const componentRoot = document.createElement("div");
+      componentRoot.setAttribute("class","card");     
+
+      const cardLeft = document.createElement("div");
+      cardLeft.setAttribute("class", "card__left");
+
+      const cardRight = document.createElement("div");
+      cardLeft.setAttribute("class", "card__right");
+
+      componentRoot.appendChild(cardLeft);
+      componentRoot.appendChild(cardRight);
+
+      return componentRoot;
+    }
 
     style(){}
 
