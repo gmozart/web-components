02a70116ac61acb6e2214f1bbeabae4c0b0e0f7e
Gleison Mozart <mozart.gleison@gmail.com>
2022-10-28 10:18:38 -0300

    Inicializado projetico prático com web-components

---
+++ src/Components/CardNews.js
@@ -0,0 +1,15 @@
+class CardNews extends HTMLHtmlElement{
+
+    constructor(){
+    super();
+
+    const shadow = this.attachShadow({mode:'open'});
+   
+
+    }
+
+    build(){}
+
+    style(){}
+
+}
